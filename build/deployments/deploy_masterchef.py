ea700dcfe04bfb9bbd94b9be803d892ad3b1e54b
FoxTheFarmer <FoxTheFarmer@cryptina.info>
2021-11-19 16:50:55 -0700

    updates

--- build/deployments/deploy_masterchef.py
+++ build/deployments/deploy_masterchef.py
@@ -16,14 +16,42 @@ MIS/RVRS - 0x14eC453656Ce925C969eafFcD76d62ACA2468Eb6
 BondPool - UST/RVRS - 0x37B380C2593a172e92a5f0BbAcA3BCfc9091060B
 """
 
-
+import time
 from build.deployments.deploy_utils import setup_mainnet_accounts
 
 Accs = setup_mainnet_accounts()
 
+frontend_pool_str = """
+  {{
+    sousId: ??,
+    tokenName: '??',
+    tokenPoolAddress: '0xED0B4b0F0E2c17646682fc98ACe09feB99aF3adE',
+    quoteTokenSymbol: QuoteToken.RVRS,
+    quoteTokenPoolAddress: '0xED0B4b0F0E2c17646682fc98ACe09feB99aF3adE',
+    stakingTokenName: QuoteToken.??,
+    stakingTokenAddress: '{want_address}',
+    contractAddress: {{
+      1666700000: '{contract_address}',
+      1666600000: '{contract_address}',
+    }},
+    poolCategory: PoolCategory.CORE,
+    projectLink: '',
+    harvest: true,
+    tokenPerBlock: '{rew_per_block}',
+    sortOrder: 1,
+    isFinished: false,
+    isDepositFinished: false,
+    startBlock: {start_block},
+    endBlock: {end_block},
+    lockBlock: {end_block},
+    tokenDecimals: 18,
+  }},
+"""
+
 
 LP_UST_RVRS = "0xF8838fcC026d8e1F40207AcF5ec1DA0341c37fe2"
 LP_RVRS_ONE = "0xCDe0A00302CF22B3Ac367201FBD114cEFA1729b4"
+LP_ETH_RVRS = "0xd1af43eb1d14b0377fbe35d2Bfadab16b96c0911"
 UST = "0x224e64ec1BDce3870a6a6c777eDd450454068FEC"
 RVRS = "0xED0B4b0F0E2c17646682fc98ACe09feB99aF3adE"
 
@@ -42,6 +70,13 @@ chef = Contract.from_abi("CoffinMakerV2", masterchef, abi=CoffinMakerV2.abi)
 vault = Contract.from_abi("AutoRvrs", auto_rvrs, abi=AutoRvrs.abi)
 vault2= Contract.from_abi("AutoRvrs", auto_rvrs2, abi=AutoRvrs.abi)
 
+
+# Update pools
+# chef.setPool(1, 15, 0, 0, 0, True, {'from': Accs.deployer})
+# chef.setPool(3, 2, 0, 0, 0, True, {'from': Accs.deployer})
+
+print(chef.poolInfo(1))
+print(chef.poolInfo(3))
 
 # ust_bond = Contract.from_abi("BondingPool", '', abi=BondingPool.abi)
 # lp_bond = Contract.from_abi("BondingPool", '', abi=BondingPool.abi)
@@ -153,3 +188,82 @@ print(f"Dev balance = {dev_bal}")
 
 resp = bond_pool.transact(dev_bal, {'from': Accs.dev})
 
+###################################
+#
+
+wantAddress = LP_UST_RVRS
+burnRate = 5000  # 50%
+rew_per_block = 1000000000000000000  # 1/block
+n_days = 5
+
+start_block = int(time.time() + 5400)  # 3 hour offset
+lock_block = int(start_block + 30*60*24*n_days)
+end_block = lock_block + 1
+
+print(f"Deploying bond pool for\n"
+      f" want:\t{wantAddress}"
+      f" burn:\t{burnRate/100:.2f}%\n"
+      f" rew:\t{rew_per_block/1e18:.3f}\n"
+      f" start:\t{start_block}\n"
+      f" end:\t{end_block}")
+
+bond_pool = BondingPool.deploy(
+    wantAddress,
+    rvrs,
+    rew_per_block,
+    start_block,
+    lock_block,
+    end_block,
+    multisig,
+    burnRate,
+    {'from': Accs.deployer}
+)
+
+
+################################
+
+wantAddress = LP_ETH_RVRS
+burnRate = 5000  # 50%
+rew_per_block = 1200000000000000000  # 1/block
+n_days = 5
+
+start_block = int(chain.height + 7000)  # 4 hour offset
+lock_block = int(start_block + 30*60*24*n_days)
+end_block = lock_block + 1
+
+print(f"Deploying bond pool for\n"
+      f" want:\t{wantAddress}\n"
+      f" burn:\t{burnRate/100:.2f}%\n"
+      f" rew:\t{rew_per_block/1e18:.3f}\n"
+      f" start:\t{start_block}\n"
+      f" end:\t{end_block}")
+
+
+bond_pool = BondingPool.deploy(
+    wantAddress,
+    rvrs,
+    rew_per_block,
+    start_block,
+    lock_block,
+    end_block,
+    multisig,
+    burnRate,
+    {'from': Accs.deployer}
+)
+
+print(f"WARNING: PLEASE SEND {(end_block-start_block)*rew_per_block/1e18:.2f} RVRS to {bond_pool.address}\n")
+
+print(frontend_pool_str.format(
+    want_address=wantAddress,
+    contract_address=bond_pool.address,
+    rew_per_block=str(rew_per_block/1e18)[:4],
+    start_block=start_block,
+    end_block=end_block,
+))
+
+lp_token = interface.ERC20(wantAddress)
+lp_token.approve(bond_pool, int(1e33), {'from': Accs.dev})
+dev_bal = lp_token.balanceOf(Accs.dev)
+print(f"Dev balance = {dev_bal}")
+
+resp = bond_pool.transact(dev_bal, {'from': Accs.dev})
